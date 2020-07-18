e9397e82a52834c10568ec34633566cad385c25d
shrikant patel <>
2020-07-18 08:30:29 -0500

    log error

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,11 +1,12 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
-const connectionURL = 'mongodb+srv://admin:Mongodbtest@cluster0-mrh2p.mongodb.net/test?retryWrites=true&w=majority' 
+const connectionURL = 'mongodb+srv://admin:Mongodbtest@cluster0.mrh2p.mongodb.net/test?retryWrites=true&w=majority' 
 const databaseName = 'task-manager'
 
 MongoClient.connect(connectionURL, { useUnifiedTopology: true, useNewUrlParser: true }, (error, client) => {
     if (error) {
+        console.log(error);
         return console.log('Error connecting to database!');
     } 
 
